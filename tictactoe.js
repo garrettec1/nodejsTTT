e40bdd2d41710768703a0041031cb41ba38cba9e
Garrett Fuller <garrettec1@gmail.com>
2015-06-17 14:07:07 -0700

    Add Game_Board object

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,8 @@
 //Command Line Tic-Tac-Toe game
 //Garrett Fuller
 //Authored June 2015
+// rough mock up of board object
+// constructor
+function Game_Board() {
+    this.board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+}
