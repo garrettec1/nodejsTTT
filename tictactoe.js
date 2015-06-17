3e380dfbdd9c7930422bc82e8136f0d7890c9dbb
Garrett Fuller <garrettec1@gmail.com>
2015-06-17 14:09:04 -0700

    Add diplay_board function

--- tictactoe.js
+++ tictactoe.js
@@ -9,3 +9,17 @@ var util = require('util');
 function Game_Board() {
     this.board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 }
+// class methods
+// going to do this in the most straight forward possible way first
+Game_Board.prototype.display_board = function(){
+    //console.trace();
+    console.log(util.format("\n\t  a   b   c"));
+    console.log(util.format('\t1 %s | %s | %s', this.board[0], this.board[1],
+                            this.board[2]));
+    console.log(util.format('\t  ==|===|=='));
+    console.log(util.format('\t2 %s | %s | %s', this.board[3], this.board[4],
+                            this.board[5]));
+    console.log(util.format('\t  ==|===|=='));
+    console.log(util.format('\t3 %s | %s | %s', this.board[6], this.board[7],
+                            this.board[8]));
+};
