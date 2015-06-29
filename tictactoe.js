07a0e59f80b2bfe1aca7a26913f61275ceeeef10
Garrett Fuller <garrettec1@gmail.com>
2015-06-29 14:23:06 -0700

    Move check move and find index to Player class.

--- tictactoe.js
+++ tictactoe.js
@@ -25,34 +25,7 @@ Game_Board.prototype.display_board = function(){
                             this.board[8]));
 };
 
-// Takes a valid move string eg '1a' and converts it to the index of board[]
-Game_Board.prototype._find_index = function(move){
-    var row = move.charAt(0);
-    var column = move.charAt(1);
-    var board_index = 0;
 
-    board_index = board_index + (row-1)*3;
-
-    if (column == 'a'){
-        board_index = board_index + 0;
-    }else if (column == 'b'){
-        board_index = board_index + 1;
-    }else if (column == 'c'){
-        board_index = board_index + 2;
-    }
-    return(board_index);
-};
-
-//Some repeated code here for row and column. Function it?
-// Checks for valid move input: is this is a move that exists on the board?
-Game_Board.prototype._check_move = function(move){
-    var row = move.charAt(0);
-    var column = move.charAt(1);
-    // I am aware there are some really cool functional ways to do this.
-    // I will try to figure them out later.
-    return((row == 1 || row == 2 || row == 3) &&
-           (column == 'a' || column == 'b' || column =='c'));
-};
 
 // Checks to ensure the space is empty and available to take a move.
 Game_Board.prototype._is_move_index_empty = function(move_index){
@@ -84,6 +57,35 @@ function Player(name, piece){
     this.piece = 'X' || 'O';
 }
 
+
+// Takes a valid move string eg '1a' and converts it to the index of board[]
+Player.prototype._find_index = function(move){
+    var row = move.charAt(0);
+    var column = move.charAt(1);
+    var board_index = 0;
+
+    board_index = board_index + (row-1)*3;
+
+    if (column == 'a'){
+        board_index = board_index + 0;
+    }else if (column == 'b'){
+        board_index = board_index + 1;
+    }else if (column == 'c'){
+        board_index = board_index + 2;
+    }
+    return(board_index);
+};
+
+//Some repeated code here for row and column. Function it?
+// Checks for valid move input: is this a valid coordinate?
+Player.prototype._check_move = function(move){
+    var row = move.charAt(0);
+    var column = move.charAt(1);
+    // I am aware there are some really cool functional ways to do this.
+    // I will try to figure them out later.
+    return((row == 1 || row == 2 || row == 3) &&
+           (column == 'a' || column == 'b' || column =='c'));
+};
 
 function Game(){
 }
