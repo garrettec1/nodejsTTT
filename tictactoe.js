fa8a7c5d1c3387544f8381c96c7b47ff3bcb433a
Garrett Fuller <garrettec1@gmail.com>
2015-09-06 20:42:40 -0500

    Change detect win to remove dependence on piece.

--- tictactoe.js
+++ tictactoe.js
@@ -77,19 +77,15 @@ Game_Board.prototype.detect_tie = function(){
 // loop win conditions
 // if there is a piece, check if all 3 are the same
 // if not break
-Game_Board.prototype.detect_win = function(piece){
-    for(set of this.win_conditions){
-        var counter = 0;
-        for(square of set){
-            if (this.board[square] == piece){
-                counter ++;
-            }
-            if (counter == 3){
-                return(true);
-            }
+Game_Board.prototype.detect_win = function(){
+    for(index_set of this.win_conditions){
+        var test_set = this.build_set(index_set);
+        var is_win = this.three_in_a_row(test_set);
+        if(is_win){
+            return(is_win);
         }
     }
-    return(false);
+    return(is_win);
 };
 
 // Checks if a set of three contains all of the same element, but not blank
